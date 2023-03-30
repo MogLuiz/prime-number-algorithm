1a1eb93c571f642368b44be6216232959be1edbd
Luiz Henrique <luizhjramos@outlook.com>
2023-03-29 21:51:56 -0300

    feat: algorithm to identify if a given number is prime

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 0
+
+	_, msg := isPrime(n)
+
+	fmt.Println(msg)
+}
+
+func isPrime(number int) (bool, string) {
+	if number == 0 || number == 1 {
+		return false, fmt.Sprintf("%d is not prime, by definition", number)
+	}
+
+	if number < 0 {
+		return false, "Negative numbers are not prime, by definition!"
+	}
+
+	for i := 2; i <= number/2; i++ {
+		if number%i == 0 {
+			return false, fmt.Sprintf("%d is not a prime number because it is divisible by %d", number, i)
+		}
+	}
+
+	return true, fmt.Sprintf("%d is a prime number!", number)
+}
